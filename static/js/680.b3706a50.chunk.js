51b1a172867cd4e14f63a2c788e0106815cb483f
vladdengoit <114827862+vladdengoit@users.noreply.github.com>
2024-02-24 18:06:03 +0000

    Deploying to gh-pages from @ vladdengoit/goit-react-hw-08-phonebook@9ef2c1f1393713e25e88e4725cdbcef6aa471e79 🚀

---
+++ static/js/680.b3706a50.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Home page"})})}}}]);
+//# sourceMappingURL=680.b3706a50.chunk.js.map
